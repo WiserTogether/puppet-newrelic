f547ed4ad51bc7d9261b63938014de589e6a23c5
Erik LaBianca <erik.labianca@wisertogether.com>
2012-07-10 19:01:37 +0000

    comment out explicit paths for apt-get so centos works

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,8 +1,8 @@
 class newrelic::repo {
-    Exec['newrelic-add-apt-key', 'newrelic-add-apt-repo', 'newrelic-apt-get-update'] {
-      path +> ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
-    }
     case $operatingsystem {
+        #Exec['newrelic-add-apt-key', 'newrelic-add-apt-repo', 'newrelic-apt-get-update'] {
+        #     path +> ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin']
+        #}
         /Debian|Ubuntu/: {
             exec { newrelic-add-apt-key:
                 unless  => "apt-key list | grep -q 1024D/548C16BF",
